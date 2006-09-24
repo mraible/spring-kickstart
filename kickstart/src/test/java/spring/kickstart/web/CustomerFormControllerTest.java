cbb9f836436d1cc6cdd1fcadf9bb30096d38cd67
Matt Raible <matt@raibledesigns.com>
2006-09-24 21:28:12 +0000

    Added test to show error that's happening on the UI

--- kickstart/src/test/java/spring/kickstart/web/CustomerFormControllerTest.java
+++ kickstart/src/test/java/spring/kickstart/web/CustomerFormControllerTest.java
@@ -3,21 +3,30 @@ package spring.kickstart.web;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 import junit.framework.TestCase;
 import spring.kickstart.domain.CustomerServiceMock;
 import spring.kickstart.domain.Customer;
+import spring.kickstart.domain.CustomerService;
+
+import java.util.Date;
 
 /**
  * @author mraible
  */
-public class CustomerFormControllerTest extends TestCase {
+public class CustomerFormControllerTest extends AbstractDependencyInjectionSpringContextTests {
     private CustomerFormController form;
 
-    public void setUp() throws Exception {
-        form = new CustomerFormController(new CustomerServiceMock());
+    public void setCustomerFormController(CustomerFormController form) {
+        this.form = form;
+    }
+
+    protected String[] getConfigLocations() {
+        return new String[]{"file:**/kickstart-servlet.xml", "repository-config.xml"};
     }
 
     public void testEdit() throws Exception {
+        form = new CustomerFormController(new CustomerServiceMock());
         // verify controller can grab user
         MockHttpServletRequest request = new MockHttpServletRequest("GET", "");
         request.addParameter("id", "1");
@@ -26,5 +35,12 @@ public class CustomerFormControllerTest extends TestCase {
         assertEquals("customer", form.getCommandName());
         Customer customer = (Customer) mv.getModel().get(form.getCommandName());
         assertEquals(new Long(1), customer.getId());
+    }
+
+    public void testAddNewCustomer() throws Exception {
+        CustomerService service = (CustomerService) applicationContext.getBean("customerService");
+        Customer customer = new Customer(null, "Chipotle", new Date());
+        service.addNewCustomer(customer);
+        assertNotNull(customer.getId());
     }
 }
