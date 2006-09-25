ae351303a9ea0f8037a4f86010db056bd22026f1
Thomas Risberg <trisberg@springdeveloper.com>
2006-09-25 12:09:42 +0000

    moved HSQL db path to point to db rather than target/db - this way the test data will survive complete rebuilds.  Also added a dbguis.sh to connect to a running database on port 9101.

--- kickstart/dbgui.sh
+++ kickstart/dbgui.sh
@@ -1,1 +1,1 @@
-java -cp ~/.m2/repository/hsqldb/hsqldb/1.8.0.5/hsqldb-1.8.0.5.jar org.hsqldb.util.DatabaseManager -url jdbc:hsqldb:./target/db/KICKSTART
+java -cp ~/.m2/repository/hsqldb/hsqldb/1.8.0.5/hsqldb-1.8.0.5.jar org.hsqldb.util.DatabaseManager -url jdbc:hsqldb:./db/KICKSTART
